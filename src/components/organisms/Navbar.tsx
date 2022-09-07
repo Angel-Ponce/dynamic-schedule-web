963727c1ac771c2c9516017ef60bbba65856d998
Angel Ponce <acdpf000@gmail.com>
2022-09-07 15:55:12 -0600

    style navbar

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -4,19 +4,14 @@ import { CalendarClear } from "react-ionicons";
 
 const Navbar: React.FC<{ hidden: boolean }> = ({ hidden }) => {
   return (
-    <MantineNavbar width={{ md: 250 }} hiddenBreakpoint="md" hidden={hidden}>
-      <MantineNavbar.Section mt="xs">
-        <Group position="apart">
-          <Group position="left">
-            <ThemeIcon radius="xl" size="xl" variant="light">
-              <CalendarClear width="20px" height="20px" />
-            </ThemeIcon>
-            <Text size="xl">Horario Dinámico</Text>
-          </Group>
-        </Group>
-      </MantineNavbar.Section>
-
-      <MantineNavbar.Section grow mx="-xs" px="xs">
+    <MantineNavbar
+      px="xl"
+      py="xl"
+      width={{ md: 250 }}
+      hiddenBreakpoint="md"
+      hidden={hidden}
+    >
+      <MantineNavbar.Section grow>
         {/* scrollable content here */}
       </MantineNavbar.Section>
 
