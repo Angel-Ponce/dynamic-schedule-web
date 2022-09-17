8940ebacb68a4e74f63137144da24ccb793a8541
Angel Ponce <acdpf000@gmail.com>
2022-09-17 14:53:29 -0600

    initial layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,17 @@
-import { AppShell } from "@mantine/core";
+import { AppShell, Grid, Box, Text, Stack, Group, Center } from "@mantine/core";
 import type { NextPage } from "next";
 import { Navbar, Header } from "$organisms";
 import { useState } from "react";
+
+const columns = [
+  "Lunes",
+  "Martes",
+  "Miércoles",
+  "Jueves",
+  "Viernes",
+  "Sábado",
+  "Domingo",
+];
 
 const Index: NextPage = () => {
   let [hidden, setHidden] = useState(true);
@@ -12,7 +22,41 @@ const Index: NextPage = () => {
       navbar={<Navbar hidden={hidden} />}
       header={<Header setHiddenNavbar={setHidden} hiddenNavbar={hidden} />}
     >
-      Schedule go here
+      <Center sx={{ width: "100%" }}>
+        <Box
+          sx={{
+            width: "100%",
+            maxWidth: "1300px",
+            display: "flex",
+          }}
+        >
+          <Box sx={{ minWidth: "135px" }}>
+            <Box sx={{ width: "100%", border: "1px solid lightgray" }} py="xs">
+              <Text align="center">Hora</Text>
+            </Box>
+          </Box>
+          <Box sx={{ flexGrow: 1, overflowX: "auto" }}>
+            <Grid columns={7} gutter={0} sx={{ flexWrap: "nowrap" }}>
+              {columns.map((col) => {
+                return (
+                  <Grid.Col
+                    key={`column-${col}`}
+                    span={1}
+                    sx={{ minWidth: "130px" }}
+                  >
+                    <Box
+                      py="xs"
+                      sx={{ width: "100%", border: "1px solid lightgray" }}
+                    >
+                      <Text align="center">{col}</Text>
+                    </Box>
+                  </Grid.Col>
+                );
+              })}
+            </Grid>
+          </Box>
+        </Box>
+      </Center>
     </AppShell>
   );
 };
