1915a2b837c686057b0b973938bf53d1afc6cb68
Angel Ponce <acdpf000@gmail.com>
2022-09-30 09:02:18 -0600

    prevent load general data without user

--- src/components/templates/LoadGeneralData.tsx
+++ src/components/templates/LoadGeneralData.tsx
@@ -1,8 +1,17 @@
-import { useSchedules } from "$hooks";
+import { useAppSelector, useSchedules } from "$hooks";
 import React, { type ReactNode } from "react";
 import { GeneralLoader } from "$templates";
+import { emptyUser } from "$slices/userSlice";
 
 const LoadGeneralData: React.FC<{ children: ReactNode }> = ({ children }) => {
+  const user = useAppSelector((state) => state.user);
+
+  if (emptyUser(user)) return <>{children}</>;
+
+  return <GeneralData>{children}</GeneralData>;
+};
+
+const GeneralData: React.FC<{ children: ReactNode }> = ({ children }) => {
   const [loading] = useSchedules();
 
   if (loading) return <GeneralLoader />;
