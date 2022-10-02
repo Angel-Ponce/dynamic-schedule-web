40b5d36312844923aa7bceb9e9ce5429397a9844
Angel Ponce <acdpf000@gmail.com>
2022-10-01 22:24:58 -0600

    scrolling settings

--- src/components/organisms/Settings.tsx
+++ src/components/organisms/Settings.tsx
@@ -10,6 +10,8 @@ import {
   Box,
   useMantineColorScheme,
   Text,
+  TextInput,
+  ScrollArea,
 } from "@mantine/core";
 import {
   IoLanguageOutline,
@@ -41,92 +43,95 @@ const Settings: React.FC<{
       overlayBlur={4}
       overlayOpacity={0.5}
     >
-      <Stack>
-        <Select
-          label="Idioma"
-          data={[{ value: "es", label: "Español" }]}
-          transition="slide-down"
-          icon={<IoLanguageOutline />}
-          value="es"
-        />
-        <Select
-          label="Tipo de letra"
-          data={[
-            { value: "default", label: "Por defecto" },
-            { value: "poppins", label: "Poppins" },
-            { value: "abyssinicasil", label: "Abyssinica SIL" },
-            { value: "ubuntu", label: "Ubuntu" },
-            { value: "heebo", label: "Heebo" },
-            { value: "lobster", label: "Lobster" },
-            { value: "pacifico", label: "Pacifico" },
-            { value: "shadowsintolight", label: "Shadows Into Light" },
-            { value: "caveat", label: "Caveat" },
-          ]}
-          transition="slide-down"
-          icon={<IoText />}
-          {...form.getInputProps("fontFamily")}
-        />
-        <Text
-          sx={{
-            fontFamily:
-              form.values.fontFamily == "default"
-                ? undefined
-                : form.values.fontFamily,
-          }}
-        >
-          Ej. Tipo de fuente
-        </Text>
-        <Select
-          label="Tema"
-          data={[
-            { value: "light", label: "Claro" },
-            { value: "dark", label: "Oscuro" },
-          ]}
-          transition="slide-down"
-          icon={<IoContrast />}
-          value={colorScheme}
-          onChange={(value) => {
-            if (value == "dark" || value == "light") toggleColorScheme(value);
-          }}
-        />
-        <Stack spacing="xl" mt="md">
-          <Checkbox id="showGrid" label="Ver celdas" />
-          <Checkbox id="hiddeSaturday" label="Ocultar día Sábado" />
-          <Checkbox id="hiddeSunday" label="Ocultar día Domingo" />
-          <Checkbox id="hiddeWeek" label="Ocultar Lunes a Viernes" />
-          <Group align="center" spacing={4}>
-            <Checkbox id="sendNotifications" label="Notificaciones push" />
-            <Tooltip
-              label="Envía una alerta cuando un curso se aproxima"
-              position="top"
-              withArrow
-            >
-              <Box sx={{ display: "flex", alignItems: "center" }}>
-                <IoHelpCircle width={18} height={18} />
-              </Box>
-            </Tooltip>
-          </Group>
-          <Group align="center" spacing={4}>
-            <Checkbox
-              id="sendEmailNotifications"
-              label="Notificaciones por correo"
-              disabled
-            />
-            <Tooltip
-              label="Envía un correo cuando un curso se aproxima"
-              position="top"
-              withArrow
-            >
-              <Box sx={{ display: "flex", alignItems: "center" }}>
-                <IoHelpCircle width={18} height={18} />
-              </Box>
-            </Tooltip>
-          </Group>
-          <Group position="right">
-            <Button sx={{ width: "fit-content" }}>Guardar</Button>
-          </Group>
+      <ScrollArea.Autosize maxHeight="calc(100vh - 100px)">
+        <Stack>
+          <TextInput label="Nombre" placeholder="Nombre de horario" />
+          <Select
+            label="Idioma"
+            data={[{ value: "es", label: "Español" }]}
+            transition="slide-down"
+            icon={<IoLanguageOutline />}
+            value="es"
+          />
+          <Select
+            label="Tipo de letra"
+            data={[
+              { value: "default", label: "Por defecto" },
+              { value: "poppins", label: "Poppins" },
+              { value: "abyssinicasil", label: "Abyssinica SIL" },
+              { value: "ubuntu", label: "Ubuntu" },
+              { value: "heebo", label: "Heebo" },
+              { value: "lobster", label: "Lobster" },
+              { value: "pacifico", label: "Pacifico" },
+              { value: "shadowsintolight", label: "Shadows Into Light" },
+              { value: "caveat", label: "Caveat" },
+            ]}
+            transition="slide-down"
+            icon={<IoText />}
+            {...form.getInputProps("fontFamily")}
+          />
+          <Text
+            sx={{
+              fontFamily:
+                form.values.fontFamily == "default"
+                  ? undefined
+                  : form.values.fontFamily,
+            }}
+          >
+            Ej. Tipo de fuente
+          </Text>
+          <Select
+            label="Tema"
+            data={[
+              { value: "light", label: "Claro" },
+              { value: "dark", label: "Oscuro" },
+            ]}
+            transition="slide-down"
+            icon={<IoContrast />}
+            value={colorScheme}
+            onChange={(value) => {
+              if (value == "dark" || value == "light") toggleColorScheme(value);
+            }}
+          />
+          <Stack spacing="xl" mt="md">
+            <Checkbox id="showGrid" label="Ver celdas" />
+            <Checkbox id="hiddeSaturday" label="Ocultar día Sábado" />
+            <Checkbox id="hiddeSunday" label="Ocultar día Domingo" />
+            <Checkbox id="hiddeWeek" label="Ocultar Lunes a Viernes" />
+            <Group align="center" spacing={4}>
+              <Checkbox id="sendNotifications" label="Notificaciones push" />
+              <Tooltip
+                label="Envía una alerta cuando un curso se aproxima"
+                position="top"
+                withArrow
+              >
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                  <IoHelpCircle width={18} height={18} />
+                </Box>
+              </Tooltip>
+            </Group>
+            <Group align="center" spacing={4}>
+              <Checkbox
+                id="sendEmailNotifications"
+                label="Notificaciones por correo"
+                disabled
+              />
+              <Tooltip
+                label="Envía un correo cuando un curso se aproxima"
+                position="top"
+                withArrow
+              >
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                  <IoHelpCircle width={18} height={18} />
+                </Box>
+              </Tooltip>
+            </Group>
+            <Group position="right">
+              <Button sx={{ width: "fit-content" }}>Guardar</Button>
+            </Group>
+          </Stack>
         </Stack>
-      </Stack>
+      </ScrollArea.Autosize>
     </Drawer>
   );
 };
