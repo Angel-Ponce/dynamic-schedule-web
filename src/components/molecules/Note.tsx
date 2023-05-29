3787964d21637387ea879e6650167b48f911fe5f
Angel Ponce <acdpf000@gmail.com>
2023-05-29 11:59:06 -0600

    prepare layout

--- src/components/molecules/Note.tsx
+++ src/components/molecules/Note.tsx
@@ -1,21 +1,33 @@
 import { Note } from "$types";
 import { FC } from "react";
-import { Box, Title, Text } from "@mantine/core";
+import { Box, Title, Text, Skeleton } from "@mantine/core";
+import { Chance } from "chance";
 
-const Note: FC<{ note: Note; loading?: boolean }> = ({
+const Note: FC<{ note?: Note; loading?: boolean }> = ({
   note,
   loading = false,
 }) => {
+  if (loading) {
+    return (
+      <Skeleton
+        className="w-60 rounded-xl my-3"
+        sx={{ height: new Chance().integer({ min: 100, max: 300 }) }}
+      />
+    );
+  }
+
   return (
-    <Box className="w-64 h-auto rounded-xl flex flex-col gap-2 p-6 relative">
-      <Title order={4}>{note.title}</Title>
-      <Text>{note.content}</Text>
-      {note.important && (
+    <Box className="min-w-[240px] min-h-[100px] max-h-[300px] overflow-auto h-auto rounded-xl inline-flex flex-col gap-2 p-6 relative my-3">
+      <Title className="max-w-max" order={4}>
+        {note?.title}
+      </Title>
+      <Text className="max-w-max">{note?.content}</Text>
+      {note?.important && (
         <Box className="absolute -top-1 -right-1 rounded-full w-3 h-3 bg-blue-600"></Box>
       )}
       <Box
         className="absolute w-full h-full top-0 left-0 -z-10 opacity-30 rounded-xl"
-        sx={{ backgroundColor: note.color }}
+        sx={{ backgroundColor: note?.color }}
       />
     </Box>
   );
--- src/pages/notes/index.tsx
+++ src/pages/notes/index.tsx
@@ -1,4 +1,4 @@
-import { AppShell } from "@mantine/core";
+import { AppShell, Box } from "@mantine/core";
 import type { NextPage } from "next";
 import { Navbar, Header } from "$organisms";
 import { useState } from "react";
@@ -15,9 +15,24 @@ const Notes: NextPage = () => {
       navbar={<Navbar hidden={hidden} />}
       header={<Header setHiddenNavbar={setHidden} hiddenNavbar={hidden} />}
     >
-      {notes?.map((note) => (
-        <Note key={note.uid} note={note}></Note>
-      ))}
+      <Box className="w-full columns-1 sm:columns-2 lg:columns-3 xl:columns-4 2xl:columns-5 3xl:columns-6">
+        {loading ||
+          (error && (
+            <>
+              {new Array(20).fill({}).map((_n, i) => (
+                <Note key={i} loading></Note>
+              ))}
+            </>
+          ))}
+
+        {!error && !loading && (
+          <>
+            {notes?.map((note) => (
+              <Note key={note.uid} note={note}></Note>
+            ))}
+          </>
+        )}
+      </Box>
     </AppShell>
   );
 };
