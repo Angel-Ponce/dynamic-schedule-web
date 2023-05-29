2d133ea5817d1f856458d962b59eba2bec35a941
Angel Ponce <acdpf000@gmail.com>
2023-05-29 09:02:20 -0600

    firebase measurement id

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const nextConfig = {
     FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
     FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
     FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    FIREBASE_MEASUREMENT_ID: process.env.FIREBASE_MEASUREMENT_ID,
   },
   images: {
     domains: ["lh3.googleusercontent.com", "googleusercontent.com"],
